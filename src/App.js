8cc07e8d09b59213208452682bb32ac79805947b
arjad <arjadgohar14@gmail.com>
2021-12-31 17:14:09 +0500

    test on mobile also

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect, useRef, useState } from "react";
+import { scroller } from "react-scroller";
+import Conversation from "./compoents/Conversation";
+
+function App() {
+  const alanBtnInstance = useRef(null);
+  useEffect(() => {
+    if (!alanBtnInstance.current) {
+      alanBtnInstance.current = alanBtn({
+        key: "5e9613e512e16b9065870de84981879c2e956eca572e1d8b807a3e2338fdd0dc/stage",
+        onCommand: (commandData) => {
+          if (commandData.command === "gotoFaq") {
+            scroller.scrollTo(`MuiPaper-${commandData.qId}`, {
+              duration: 800,
+              delay: 0,
+              smooth: "easeInOutQuart",
+            });
+          } else if (commandData.command === "openMyYoutube") {
+            // window.location.href = "https://youtube.com/c/CodeWithAkky";
+            window.open("https://youtube.com/c/CodeWithAkky", "_blank");
+          } else if (commandData.command === "openSourceCode") {
+            // window.location.href =
+            //   "https://github.com/akkySrivastava/chat-bot-ai";
+            window.open(
+              "https://github.com/akkySrivastava/chat-bot-ai",
+              "_blank"
+            );
+          }
+        },
+      });
+    }
+  }, []);
+  return (
+    <div className="App">
+      <Conversation />
+    </div>
+  );
+}
+
+export default App;
